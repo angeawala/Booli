12d7233f9f862ef581e74b1fc4f289c9c678e323
AWALA Ange <awala@Booli.e-booli.com>
2025-03-15 20:27:04 +0100

    Devise ajouter au admin

--- api/product/admin.py
+++ api/product/admin.py
@@ -7,7 +7,7 @@ from .models import (
 
 @admin.register(BaseProduct)
 class BaseProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'subcategory', 'price', 'created_by', 'created_at')
+    list_display = ('name', 'category', 'subcategory', 'devise', 'price', 'created_by', 'created_at')
     list_filter = ('category', 'subcategory', 'created_by')
     search_fields = ('name', 'category__name', 'subcategory__name')
     list_per_page = 25
